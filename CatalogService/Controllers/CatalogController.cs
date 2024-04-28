c2105fe984422907237679052938a22ed84f5910
sanaparveen <sana.parveen@nagarro.com>
2024-04-28 20:13:27 +0530

    pus

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using CatalogService.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        DatabaseContext _db;
+        public CatalogController(DatabaseContext db) 
+        {
+            //lets do some changes
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+        
+    }
+}
---
+++ CatalogService/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        //checking jenkins pipeline
+        public DbSet<Product>? Products { get; set; }
+    }
+}
